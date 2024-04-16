49d66612c12f3d995dff3335b2464f4d39ca7b50
Rohim Saga <you@example.com>
2024-04-16 13:41:27 +0700

    feat:add to cart

--- router/index.js
+++ router/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const UserController = require("../controller/user");
+const cartController = require("../controller/cart");
 const router = express.Router();
 
 router.get("/register", UserController.Register);
 router.get("/login", UserController.Login);
+
+// API CART
+
+router.post("/addtocart", cartController.addToCart);
 
 module.exports = router;
