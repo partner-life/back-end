45c602175fb86127178a06ca428fb0d8c27ea06d
Hackim18 <khakim668@gmail.com>
2024-04-18 00:11:34 +0700

    feat: add item & customer details during transaction

--- controller/payment.js
+++ controller/payment.js
@@ -9,7 +9,7 @@ const snap = new midtransClient.Snap({
 class PaymentController {
   static async createTransaction(req, res, next) {
     try {
-      const { gross_amount, order_id, description } = req.body;
+      const { gross_amount, order_id, item_name, first_name, last_name, phone, address, city, postal_code } = req.body;
 
       const amount = +gross_amount;
 
@@ -17,15 +17,32 @@ class PaymentController {
         throw { name: "BadRequest", message: "Amount must be a number" };
       }
 
-      if (!order_id || !description) {
-        throw { name: "BadRequest", message: "Order ID and Description are required" };
+      if (!order_id || !item_name) {
+        throw { name: "BadRequest", message: "Order ID and Item Name are required" };
       }
 
       const parameter = {
         transaction_details: {
           order_id,
           gross_amount: amount,
-          description,
+        },
+        item_details: [
+          {
+            id: order_id,
+            price: amount,
+            quantity: 1,
+            name: item_name,
+          },
+        ],
+        customer_details: {
+          first_name,
+          last_name,
+          phone,
+          shipping_address: {
+            address,
+            city,
+            postal_code,
+          },
         },
       };
 
