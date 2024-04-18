ef89e34d11a0fd9300f2ce295572615402348c0e
Rohim Saga <you@example.com>
2024-04-18 09:19:12 +0700

    fix:cart

--- model/cart.js
+++ model/cart.js
@@ -2,37 +2,47 @@ const { ObjectId } = require("mongodb");
 const database = require("../config/db");
 
 class cart {
-  static collection() {
-    return database.collection("Carts");
-  }
-  static async addToCart(productId, UserId) {
-    const id = new ObjectId(productId);
-    const cartCollection = this.collection();
-    const result = await cartCollection.insertOne({
-      ProductId: id,
-      UserId: UserId,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    return result;
-  }
-  static async findAllCart() {
-    const cart = this.collection.find({}).toArray();
-    return cart;
-  }
-  static async findCartById(cartId) {
+   static collection() {
+      return database.collection("Carts");
+   }
+   static async addToCart(productId, UserId) {
+      const id = new ObjectId(productId);
+      const cartCollection = this.collection();
+      const result = await cartCollection.insertOne({
+         ProductId: id,
+         UserId: UserId,
+         createdAt: new Date(),
+         updatedAt: new Date(),
+      });
+      return result;
+   }
+   static async findAllCart() {
+      try {
+         const cartCollection = await this.collection();
+         const carts = await cartCollection.find({}).toArray();
+         return carts;
+      } catch (error) {
+         throw new Error(error.message);
+      }
+   }
+
+   static async findCartById(cartId) {
+  try {
     const id = new ObjectId(cartId);
-    console.log(id);
-    const cart = await this.collection().findOne({ _id: id });
-    console.log(cart);
+    const cartCollection = await this.collection();
+    const cart = await cartCollection.findOne({ _id: id });
     return cart;
+  } catch (error) {
+    throw new Error(error.message);
   }
-  static async deleteproductcart(cartId) {
-    return this.collection().deleteOne({ _id: cartId });
-  }
-  static async editproductcart(cartId, updatedData) {
-    return this.collection().updateOne({ _id: cartId }, { $set: updatedData });
-  }
+}
+
+   static async deleteproductcart(cartId) {
+      return this.collection().deleteOne({ _id: cartId });
+   }
+   static async editproductcart(cartId, updatedData) {
+      return this.collection().updateOne({ _id: cartId }, { $set: updatedData });
+   }
 }
 
 module.exports = cart;
