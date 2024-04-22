94d2ef0a0a6310180a090cb775806ef82d4e5fdf
abdularifin <abdul.arifin12@gmail.com>
2024-04-22 13:57:14 +0700

    fix cors

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const router = require("./router/index");
 const errorHandler = require("./middleware/errorHandler");
 const cors = require("cors");
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors);
 app.use(router);
 app.use(errorHandler);
 
