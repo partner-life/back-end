4557eb3ff45936e69235d304da5cc2c22f7ebb10
abdularifin <abdul.arifin12@gmail.com>
2024-04-22 16:02:15 +0700

    fix endpoint package

--- __test__/updateOrders.test.js
+++ __test__/updateOrders.test.js
@@ -7,36 +7,36 @@ const { signToken } = require("../helper/jwt");
 
 beforeAll(async () => {
   let data = {
-    email: "rohimjoy70@gmail.com",
-    password: "test",
+    email: "testing@mail.com",
+    password: "testing",
   };
   const login = await database
     .collection("Users")
     .findOne({ email: data.email });
   access_token = signToken({ id: login._id });
+  let order = await Orders.newOrders(userData);
 });
 
 let access_token;
-
+let order;
 const userData = {
   UserId: "6624fdb5a004ed8151377370",
-  PackagesId: "66234fdc5de01fedc08b3fcf",
+  PackageId: "66234fdc5de01fedc08b3fcf",
   status: "false",
   price: 100000000,
-  Profil: {
-    HusbandName: "Toba",
-    WifeName: "NameOfWife",
-    address: "123 Main Street",
-    phoneNumber: "123-456-7890",
-    dateOfMerried: "2024-05-01",
-  },
+
+  HusbandName: "Toba",
+  WifeName: "NameOfWife",
+  address: "123 Main Street",
+  phoneNumber: "123-456-7890",
+  dateOfMerried: "2024-05-01",
 };
 
 describe("POST /updateOrders/:orderId", () => {
   test("should be able to update order", async () => {
     // console.log(access_token)
     const response = await request(app)
-      .put("/updateOrders/6626178e5d8beda72bc16784")
+      .put("/updateOrders/" + order._id)
       .set("Authorization", "Bearer " + access_token)
       .send(userData);
     console.log(response.body);
@@ -46,7 +46,7 @@ describe("POST /updateOrders/:orderId", () => {
     const { nameHusband, ...userDataWithoutNameHusband } = userData;
 
     const response = await request(app)
-      .put("/updateOrders/662617da4c9ff35ceb2e15d9")
+      .put("/updateOrders/" + order._id)
       .set("Authorization", "Bearer " + access_token)
       .send(userDataWithoutNameHusband);
     console.log(response.body);
